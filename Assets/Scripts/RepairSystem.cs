761f463d2abc58cd1b06aa90eb2968f4670502a4
saraloncaric <167783165+saraloncaric@users.noreply.github.com>
2026-05-10 19:06:46 +0200

    Dodano za popravak i gumb

---
+++ Assets/Scripts/RepairSystem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RepairSystem : MonoBehaviour
+{
+    public EconomyManager economy;
+    public int cijenaPopravka = 5;
+
+    public void PokusajPopravak() {
+        if(economy.coins >= cijenaPopravka) {
+            economy.coins -= cijenaPopravka;
+            Debug.Log("Popravak uspješan, preostalo je još: " + economy.coins);
+        } else {
+            Debug.Log("Nema dovoljno coinsa za popravak");
+        }
+    }
+}
